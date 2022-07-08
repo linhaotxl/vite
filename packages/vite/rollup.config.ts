ff833d589b22783d2c5608127f0a324a81d62f6d
linhaotxl <602523587@qq.com>
2022-07-08 18:02:44 +0800

    build(root): 修改打包配置

--- packages/vite/rollup.config.ts
+++ packages/vite/rollup.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'rollup'
 import esbuild from 'rollup-plugin-esbuild'
 import eslint from 'rollup-plugin-eslint2'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import alias from '@rollup/plugin-alias'
+import json from '@rollup/plugin-json'
+import typescript from '@rollup/plugin-typescript'
 import path from 'path'
 
 const base = __dirname
-const resolve = (...p: string[]) =>  path.resolve(base, ...p)
+const resolve = (...p: string[]) => path.resolve(base, ...p)
 
 export default defineConfig({
-
   input: {
     index: resolve('src/node/index.ts'),
     cli: resolve('src/node/cli.ts'),
@@ -15,13 +19,39 @@ export default defineConfig({
 
   output: {
     format: 'commonjs',
-    dir: resolve('dist/node')
+    dir: resolve('dist/node'),
   },
 
   plugins: [
-    esbuild(),
+    // alias({
+    //   entries: {
+    //     '@vue/compiler': require.resolve(
+    //       '@vue/compiler-dom/dist/compiler-dom.cjs.js'
+    //     )
+    //   }
+    // }),
 
-    eslint({ fix: true, throwOnError: false })
-  ]
+    nodeResolve({
+      // preferBuiltins: true
+    }),
 
+    typescript({
+      tsconfig: resolve('src/node/tsconfig.json'),
+    }),
+
+    // esbuild({
+    //   tsconfig: resolve('tsconfig.json')
+    // }),
+
+    eslint({ fix: true, throwOnError: false }),
+
+    commonjs({
+      // extensions: ['.js'],
+      // Optional peer deps of ws. Native deps that are mostly for performance.
+      // Since ws is not that perf critical for us, just ignore these deps.
+      // ignore: ['bufferutil', 'utf-8-validate']
+    }),
+
+    json(),
+  ],
 })
