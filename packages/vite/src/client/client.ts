33445ff2651adb1f19b0a50e595d423931a3508f
linhaotxl <602523587@qq.com>
2022-08-05 14:23:44 +0800

    feat(vite): 创建import.meta.hot

--- packages/vite/src/client/client.ts
+++ packages/vite/src/client/client.ts
@@ -1,3 +1,4 @@
+import type { HotCallback, HotModule, ViteHotContext } from '../../types/hot'
 import '@vite/env'
 
 /**
@@ -16,4 +17,31 @@ export const updateStyle = (id: string, content: string) => {
   }
 
   sheetsMap.set(id, style)
+}
+
+/**
+ * 创建 import.meta.hot
+ */
+const hotModulesMap = new Map<string, HotModule>()
+export const createHotContext = (ownerPath: string): ViteHotContext => {
+  const acceptDeps = (deps: string[], fn: HotCallback['fn']) => {
+    const module = hotModulesMap.get(ownerPath) || {
+      id: ownerPath,
+      callbacks: [],
+    }
+
+    module.callbacks.push({ deps, fn })
+
+    hotModulesMap.set(ownerPath, module)
+  }
+
+  return {
+    accept(deps?, cb?) {
+      debugger
+      if (typeof deps === 'function' || typeof deps === 'undefined') {
+        // import.meta.hot.accept() / import.meta.hot.accept(() => {})
+        acceptDeps([ownerPath], () => deps?.())
+      }
+    },
+  }
 }
